0a05f92d50cb0372e08b208f3d88a0ad13099228
Prestamo Estudiantes <prestamour@urosario.edu.co>
2025-04-25 15:14:26 -0500

    está mal los icons

--- app/user/myMedicines/page.tsx
+++ app/user/myMedicines/page.tsx
@@ -1,22 +1,14 @@
 import { getSession } from "@/context/session"
-export default function MyMedicines({session}:{session: {isLoggedIn:boolean, userEmail: string }}){
-   
-    
-    return (
-        <>
-        
-        <h1>medicinas</h1>
-        <p>Bienvenido, {session.userEmail || 'Usuario'} </p>
-        </>
-    )
-}
 
-export async function getServerSideProps(){
-    const session = await getSession();
+export default async function MyMedicines() {
+  const session = await getSession()
 
-    return{
-        props:{
-            session
-        }
-    };
+  return (
+    <>
+      <h1>Medicinas</h1>
+      {session !== undefined && (
+        <p>Bienvenido, {session.userEmail || 'Usuario'}</p>
+      )}
+    </>
+  )
 }
--- components/bottommenu.tsx
+++ components/bottommenu.tsx
@@ -1,5 +1,5 @@
-import Image from 'next/image'
 import Link from 'next/link'
+import {CalendarHeart, NotebookText, Pill} from 'lucide-react'
 export default function BottomMenu(){
     //Iconos para el menu (toca declarar las rutas)
     const my_medicines = '';
@@ -10,19 +10,19 @@ export default function BottomMenu(){
         <div className="flex justify-around items-center w-full h-full bg-[#307D85] text-white">
         {/* Mis Medicinas */}
         <Link href="/user/myMedicines" className="flex flex-col items-center">
-            <Image src={my_medicines} alt="Mis Medicinas" width={24} height={24} />
+            <Pill size = {24}/>
             <span>Medicinas</span>
         </Link>
 
         {/* Calendario */}
         <Link href="/user/calendar" className="flex flex-col items-center">
-            <Image src={calendar} alt="Calendario" width={24} height={24} />
+            <CalendarHeart size = {24}/>
             <span>Calendario</span>
         </Link>
 
         {/* Historial*/}
         <Link href="/user/history" className="flex flex-col items-center">
-            <Image src={register} alt="Registro" width={24} height={24} />
+            <NotebookText size = {24}/>
             <span>Registro</span>
         </Link>
     </div>
